6961c0ab62f36a0dfacee01b5265daaa85d5ab0a
Arkadey Bolshakov <ewaypeople@gmail.com>
2023-05-08 23:57:34 +0300

    Connection AdminForm with server

--- StudyWatcherFormsAdmin/Form1.cs
+++ StudyWatcherFormsAdmin/Form1.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.SignalR.Client;
+
 namespace StudyWatcherFormsAdmin;
 
 public partial class MainForm : Form
 {
+    private readonly HubConnection _hubConnection;
     public MainForm()
     {
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:44300/StudyWatcherHub")
+            .Build();
         InitializeComponent();
     }
 }
